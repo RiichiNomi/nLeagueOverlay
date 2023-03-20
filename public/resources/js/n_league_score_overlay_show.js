fc1ea1611489a90a585c5e50f86e420edc4201f2
julian7890 <64969120+julian7890@users.noreply.github.com>
2023-03-19 20:25:12 -0400

    Add files via upload

--- public/resources/js/n_league_score_overlay_show.js
+++ public/resources/js/n_league_score_overlay_show.js
@@ -1,12 +1,59 @@
 let namebox = document.querySelectorAll('.playername');
+let waitsText = document.querySelectorAll('.waitsTextE, .waitsTextS, .waitsTextW, .waitsTextN');
 let waitsContainer = document.querySelectorAll('.waitsContainer');
-document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio));
+let doraContainer = document.querySelector('.dora');
+const socket = io('http://localhost:3001');
+document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale=' + (1 / window.devicePixelRatio));
+
+
+let fields = {};
+
+socket.on('change', (data) => {
+    console.log(data);
+    let { updateDescription } = data;
+    let { updatedFields } = updateDescription;
+    let keys = Object.keys(updatedFields);
+    fields = updatedFields;
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i].includes('waits')) {
+            let container = document.getElementById(keys[i]);
+            container.innerHTML = '';
+            let updatedValue = Object.values(fields)[i];
+            let newWaits = updatedValue.map(s => s.trim());
+            if (newWaits[0] !== "") {
+                for (let j = 0; j < newWaits.length; j++) {
+                    let img = document.createElement('img');
+                    img.src = `../public/resources/img/pai_image/${newWaits[j]}.png`
+                    container.appendChild(img);
+                };
+            }
+            toggleWaitsText(waitsText);
+            marqueeWaits(waitsContainer);
+        } else if (keys[i].includes('dora')) {
+            doraContainer.innerHTML = '';
+            let updatedValue = Object.values(fields)[i];
+            let newDora = updatedValue.map(s => s.trim());
+            if (newDora[0] !== "") {
+                for (let k = 0; k < newDora.length; k++) {
+                    let img = document.createElement('img');
+                    img.src = `../public/resources/img/pai_image/${newDora[k]}.png`
+                    doraContainer.appendChild(img);
+                };
+            }
+        } else {
+            let container = document.getElementById(keys[i]);
+            let updatedValue = Object.values(fields)[i];
+            container.innerHTML = updatedValue;
+        }
+    }
+});
+
 
 changeFontSize = (arr) => {
     for (let x of arr) {
         if (x.offsetWidth > x.parentElement.offsetWidth) {
             let compSize = getComputedStyle(x).fontSize;
-            x.style.fontSize = parseFloat(compSize)*0.85 + 'px';
+            x.style.fontSize = parseFloat(compSize) * 0.85 + 'px';
         }
     }
 };
@@ -14,17 +61,29 @@ changeFontSize = (arr) => {
 
 marqueeWaits = (arr) => {
     for (let x of arr) {
-        if(x.offsetWidth > x.parentElement.offsetWidth ){
+        if (x.offsetWidth > x.parentElement.offsetWidth) {
             x.classList.add('marquee')
+        } else {
+            x.classList.remove('marquee')
+        }
+    }
+}
+
+toggleWaitsText = (arr) => {
+    for (let x of arr) {
+        if (x.nextElementSibling.children[0].childElementCount > 0) {
+            x.style.opacity = 1;
+        } else {
+            x.style.opacity = 0;
         }
     }
 }
 
 
-
 let start = () => {
     changeFontSize(namebox);
     marqueeWaits(waitsContainer);
+    toggleWaitsText(waitsText);
 }
 
 window.onload = start;
